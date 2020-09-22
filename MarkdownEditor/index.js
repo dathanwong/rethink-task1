3a41874026ebe74d0579d9359ed3d9f48742b60c
dathanwong <dwong5@villanova.edu>
2020-09-21 18:07:32 -0700

    Added markdown rendering

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
+const ReactMarkdown = require('react-markdown');
 
 import css from "./style.css";
 
 function MarkdownEditor({ file, write }) {
-  console.log(file, write);
+  
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    console.log(file);
+    file.text()
+    .then(res =>{
+      setValue(res);
+    })
+  }, [file]);
+
+  function saveFile(e){
+    let newFile = new File(
+      [value],
+      file.name,
+      {
+        type: "text/markdown",
+        lastModified: new Date()
+      }
+    );
+    write(newFile);
+  }
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
+      <textarea rows="25" cols="70" value={value} onChange={(e) => setValue(e.target.value)}></textarea>
+      <button onClick={saveFile}>Save</button>
+      <ReactMarkdown source={value} />
     </div>
   );
 }
